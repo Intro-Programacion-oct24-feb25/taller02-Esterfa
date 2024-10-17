0740880526424117dafcdccf57d487b27541a987
Ester <estercika83@gmail.com>
2024-10-17 11:52:09 -0500

    primer commit

---
+++ proyecto01/src/proyecto01/Proyecto01.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto01;
+
+/**
+ *
+ * @author utpl
+ */
+public class Proyecto01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Ejemplo básico de Java\n" +
+"Versionado a través de Git\n" +
+"Manejado desde GitHub ");
+
+    }
+    
+}
+
